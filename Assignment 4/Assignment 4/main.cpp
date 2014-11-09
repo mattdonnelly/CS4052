d7b768c57d2669ad017cf2a5b458a316fcb045ff
Matt Donnelly <mattdonnelly@me.com>
2014-11-09 20:13:14 +0000

    Start Assignment 4

---
+++ Assignment 4/Assignment 4/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment 4
+//
+//  Created by Matt Donnelly on 09/11/2014.
+//  Copyright (c) 2014 Matt Donnelly. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
