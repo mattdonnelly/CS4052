152998aaed0eb238463c509db2d88153ae7649df
Matt Donnelly <mattdonnelly@me.com>
2014-11-18 18:34:10 +0000

    Fix formatting in shader

--- Assignment 4/Assignment 4/fragment_shader.glsl
+++ Assignment 4/Assignment 4/fragment_shader.glsl
@@ -21,18 +21,18 @@ out vec4 fragment_colour;
 void main () {
     vec3 Ia = La * Ka;
 
-    vec3 light_position_eye = vec3 (view_mat * vec4 (light_position_world, 1.0));
+    vec3 light_position_eye = vec3(view_mat * vec4(light_position_world, 1.0));
     vec3 distance_to_light_eye = light_position_eye - position_eye;
-    vec3 direction_to_light_eye = normalize (distance_to_light_eye);
-    float dot_prod = dot (direction_to_light_eye, normal_eye);
-    dot_prod = max (dot_prod, 0.0);
+    vec3 direction_to_light_eye = normalize(distance_to_light_eye);
+    float dot_prod = dot(direction_to_light_eye, normal_eye);
+    dot_prod = max(dot_prod, 0.0);
     vec3 Id = Ld * Kd * dot_prod;
 
-    vec3 reflection_eye = reflect (-direction_to_light_eye, normal_eye);
-    vec3 surface_to_viewer_eye = normalize (-position_eye);
-    float dot_prod_specular = dot (reflection_eye, surface_to_viewer_eye);
-    dot_prod_specular = max (dot_prod_specular, 0.0);
-    float specular_factor = pow (dot_prod_specular, specular_exponent);
+    vec3 reflection_eye = reflect(-direction_to_light_eye, normal_eye);
+    vec3 surface_to_viewer_eye = normalize(-position_eye);
+    float dot_prod_specular = dot(reflection_eye, surface_to_viewer_eye);
+    dot_prod_specular = max(dot_prod_specular, 0.0);
+    float specular_factor = pow(dot_prod_specular, specular_exponent);
     vec3 Is = Ls * Ks * specular_factor;
 
     vec2 flipped_tex_coord = vec2(frag_tex_coord.x, 1.0 - frag_tex_coord.y);
